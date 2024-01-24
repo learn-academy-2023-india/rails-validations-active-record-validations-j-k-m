3eeec14166b2d0317b7cfebdca7112a2add7e450
Michael Egner <148016951+MEgner410@users.noreply.github.com>
2024-01-24 14:45:01 -0800

    initial commit

--- README.md
+++ README.md
@@ -22,4 +22,3 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
-# rails-validations-active-record-validations-j-k-m
---
+++ db/migrate/20240124220443_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :addresses do |t|
+      t.integer :street_number
+      t.string :street_name
+      t.string :state
+      t.integer :zip
+
+      t.timestamps
+    end
+  end
+end
